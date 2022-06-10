90321f19f932a207fccd1af69ad009e3c36cb28b
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-06-10 13:52:42 +0200

    END exercise

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -3,9 +3,11 @@ const largeArrey = [1,2,3,4,5,6,7,8,9,10,11,12]
 const smallArrey = [1,2,3,4,5]
 
 console.log(largeArrey)
+
 // calcolo del ciclo
 while(smallArrey.length < largeArrey.length) {
   let randomvalue = Math.floor(Math.random() * 100) + 1;
   smallArrey.push(randomvalue);
+  // printo dli array mentre si popolano
   console.log(smallArrey)
 }
